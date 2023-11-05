0367f543fbad99eae571e26a145c854e5c66120c
gihozoinnocente <88516265+gihozoinnocente@users.noreply.github.com>
2023-11-05 12:37:14 +0200

    Update 3-dictionary_of_list_of_dictionaries.py

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,37 @@
 #!/usr/bin/python3
 """Exports data in the JSON format"""
 
-if __name__ == "__main__":
+import json
+import requests
 
-    import json
-    import requests
-    import sys
+if __name__ == "__main":
+    # Fetch user and task data from the JSONPlaceholder API
+    users_response = requests.get("https://jsonplaceholder.typicode.com/users")
+    todos_response = requests.get('https://jsonplaceholder.typicode.com/todos')
 
-    users = requests.get("https://jsonplaceholder.typicode.com/users")
-    users = users.json()
-    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-    todos = todos.json()
-    todoAll = {}
+    if users_response.status_code != 200 or todos_response.status_code != 200:
+        print("Failed to fetch data from the API.")
+        exit(1)
+
+    users = users_response.json()
+    todos = todos_response.json()
+
+    # Create a dictionary to store tasks for all employees
+    todo_all_employees = {}
 
     for user in users:
-        taskList = []
-        for task in todos:
-            if task.get('userId') == user.get('id'):
-                taskDict = {"username": user.get('username'),
-                            "task": task.get('title'),
-                            "completed": task.get('completed')}
-                taskList.append(taskDict)
-        todoAll[user.get('id')] = taskList
+        user_id = user['id']
+        username = user['username']
 
-    with open('todo_all_employees.json', mode='w') as f:
-        json.dump(todoAll, f)
+        # Filter tasks for the current user
+        user_tasks = [{"username": username, "task": task['title'], "completed": task['completed']}
+                     for task in todos if task['userId'] == user_id]
+
+        # Add the tasks to the dictionary
+        todo_all_employees[user_id] = user_tasks
+
+    # Write the data to a JSON file
+    with open('todo_all_employees.json', 'w') as json_file:
+        json.dump(todo_all_employees, json_file, indent=4)
+
+    print("Data exported to todo_all_employees.json")
